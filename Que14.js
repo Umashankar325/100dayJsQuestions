d4dc9672b5efc5f26d2e00e57fbea551e8a259fd
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 17:09:02 +0530

    question14

---
+++ Que14.js
@@ -0,0 +1,13 @@
+console.log("Extend the previous program and handle the wrong inputs.      Print Good Morning sir for input m or M & Good morning Maam for input F or f else print Wrong Input ");
+
+let name=prompt("enter your name ");
+let gender=prompt("enter your gender ");
+if (gender=="male"||gender=="Male") {
+  console.log("Good Morning Sir ");
+  
+} else if (gender=="male"||gender=="Male") {
+  console.log("Good Morning mam ");
+}else{
+  console.log("Wrong Input");
+  
+}
