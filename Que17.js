c4f0676a1899e14866b5bfa3c5cd2744478d7b65
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 17:25:10 +0530

    question17

---
+++ Que17.js
@@ -0,0 +1,27 @@
+console.log("Accept a day number between 1-7 and print the corresponding dayname");
+let daynum=+prompt("enter day number");
+if (daynum==1) {
+  console.log("today is monday");
+  
+}else if (daynum==2) {
+  console.log("today is tuesday");
+  
+} else if (daynum==3) {
+  console.log("today is wednesday");
+  
+}else if (daynum==4) {
+  console.log("today is tharseday");
+  
+}else if (daynum==5) {
+  console.log("today is friday");
+  
+} else if (daynum==6) {
+  console.log("today is saterday");
+  
+}else if (daynum==7) {
+  console.log("today is sunday");
+  
+}else {
+  console.log("Wrong input");
+  
+}
