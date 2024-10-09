59e52600bbc37223b8306beebea4d7bfc8024fed
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:21:33 +0530

    question4

---
+++ Que04.js
@@ -0,0 +1,5 @@
+console.log("Accept the User's name, age and print in following manner	Ex - Hello Shery, you are 12 years old.");
+let name =prompt("enter your name")
+let age =+prompt("enter your age")
+console.log(`Hello ${name}, you are ${age} years old.`);
+
