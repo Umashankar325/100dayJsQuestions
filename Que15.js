7cea30666f1c956e0ebad0dbc8be0d814dea028f
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 17:12:25 +0530

    question15

---
+++ Que15.js
@@ -0,0 +1,8 @@
+console.log("Accept an integer and check whether it is an even number or odd.");
+let num=+prompt("enter number");
+if (num%2==0) {
+  console.log("given number is even number");
+  
+} else {
+  console.log("given number is odd number");
+}
