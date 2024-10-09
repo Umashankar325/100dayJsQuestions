580438b5149026e3b10c7ce797f3649208db22b0
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:59:56 +0530

    question12

---
+++ Que12.js
@@ -0,0 +1,10 @@
+console.log("Accept two numbers and print the greatest between them");
+let num1=+prompt("enter first number")
+let num2=+prompt("enter second number")
+if (num1>num2) {
+  console.log(`${num1} is grater then ${num2}`);
+  
+}
+else{
+  console.log(`${num2} is grater then ${num1}`);
+}
