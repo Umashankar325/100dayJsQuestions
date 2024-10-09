a5625f0996a99e0bc5a31b72c565685cbeec424b
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:56:24 +0530

    question11

---
+++ Que11.js
@@ -0,0 +1,5 @@
+console.log("Find circumference and area of circle");
+let r=+prompt("enter radius  of circle")
+let aria=Math.PI*r*r;
+let circumference=2*Math.PI*r;
+console.log(`area of circle is ${aria} squre unit and circumference is ${circumference} unit` );
