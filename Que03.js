8c6c7160af6376b12ca9caae845a8ecb48a16958
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:18:17 +0530

    question3

---
+++ Que03.js
@@ -0,0 +1,6 @@
+
+console.log("Accept two integers from user and print the sum	Ex - The sum of 45 & 12 = 57 ");
+let a=+prompt("enter first number");
+let b=+prompt("enter second number");
+let sum=a+b;
+console.log(`sum of ${a} and ${b} = ${sum}`);
