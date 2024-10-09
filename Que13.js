c398c6467b4c0b463f9433d3771fe16ce8aeac4b
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 17:06:05 +0530

    question13

---
+++ Que13.js
@@ -0,0 +1,9 @@
+console.log(" Accept the gender from the user as char and print the respective greeting message Ex - Good Morning Sir (on the basis of gender) ");
+let name=prompt("enter your name ");
+let gender=prompt("enter your gender ");
+if (gender=="male"||gender=="Male") {
+  console.log("Good Morning Sir ");
+  
+} else {
+  console.log("Good Morning mam ");
+}
