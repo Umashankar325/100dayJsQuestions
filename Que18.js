66f80f46edbc31c0a867b61b013715d51bd6e8cd
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 17:29:53 +0530

    question18

---
+++ Que18.js
@@ -0,0 +1,14 @@
+console.log("Accept three numbers and print the greatest among them");
+let num1=+prompt("enter num01")
+let num2=+prompt("enter num02")
+let num3=+prompt("enter num03")
+if (num1>num2 && num1>num3) {
+  
+}if (num1>num2 && num1>num3) {
+  console.log(`largest num is ${num1}`);
+  
+} else if (num2>num1 && num2>num3) {
+  console.log(`largest num is ${num2}`);
+}  else {
+  console.log(`largest num is ${num3}`);
+}
