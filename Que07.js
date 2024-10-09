29086720c29005a033ee2f2d3688a85532451b76
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:37:01 +0530

    question7

---
+++ Que07.js
@@ -0,0 +1,7 @@
+console.log("Accept the length and width of a rectangle. Calculate & print the area and perimiter.");
+let length=+prompt("length of a rectangle");
+let width=+prompt("width of a rectangler");
+let aria=length*width;
+let perimiter=(length+width)*2;
+
+console.log(`the aria of rectangle is ${aria} and parameter of rectangle is ${perimiter}`);
