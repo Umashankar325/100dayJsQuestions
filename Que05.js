efc8ca16b7ebe6a4be3ea3a4cef83d3d6378cd4e
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-09 16:25:41 +0530

    question5

---
+++ Que05.js
@@ -0,0 +1,5 @@
+console.log("Solve Increment & decrement operator questions on https://javaconceptoftheday.com/quiz-on-increment-and-decrement-operators/");
+ let i = 0;
+ let j = 0;
+         
+console.log(--i * i++ * ++j * j++);  //output is one
