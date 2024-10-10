30ac5358d80074f8029a45ba026178c3f8f5f237
Umashnkar Uikey <umashankaruikey325@gmail.com>
2024-10-10 11:30:05 +0530

    qustion22

---
+++ Que22.js
@@ -0,0 +1,8 @@
+console.log("Accept an english alphabet from user and check if it is a consonent or a vowel");
+let char=prompt("enter charctor");
+if (char=='a'||char=='e'||char=='i'||char=='o'||char=='u'||char=='A'||char=='E'||char=='I'||char=='O'||char=='U') {
+  console.log(`given input ${char} is vowel `);
+  
+} else {
+  console.log(`given input ${char} is consonent `);
+}
---
+++ Que39.js
@@ -0,0 +1,7 @@
+let num=5;
+let sqr=num*num;
+let digit=0;
+
+while(num>0){
+  digit++;
+}
